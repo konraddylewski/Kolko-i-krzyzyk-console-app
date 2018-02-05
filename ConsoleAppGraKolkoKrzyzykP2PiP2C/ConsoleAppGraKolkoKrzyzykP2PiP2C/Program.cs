a50e4e5932deb20faeeb9741f76e2fb755e4d416
konraddylewski <konrad.dylewski@gmail.com>
2018-02-05 22:29:30 +0100

    Utworzenie drzewa projektu

---
+++ ConsoleAppGraKolkoKrzyzykP2PiP2C/ConsoleAppGraKolkoKrzyzykP2PiP2C/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGraKolkoKrzyzykP2PiP2C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
